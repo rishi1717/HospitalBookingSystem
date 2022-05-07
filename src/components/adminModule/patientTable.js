d010d95f9619b19d7ae0109aba7d55099398b43c
rishi1717 <rshrrrss@gmail.com>
2022-05-07 20:50:52 +0530

    added patient page in admin

---
+++ src/components/adminModule/patientTable.js
@@ -0,0 +1,65 @@
+import * as React from "react"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
+import { SmallButton } from "../Buttons"
+import { useNavigate } from "react-router-dom"
+
+export default function PatientTable({ users }) {
+	const navigate = useNavigate()
+	return (
+		<TableContainer component={Paper} sx={{ maxWidth: "88vw" }}>
+			<Table sx={{ minWidth: { sm: 650 } }} aria-label="simple table">
+				<TableHead>
+					<TableRow sx={{ backgroundColor: "#1976D2" }}>
+						<TableCell align="center" sx={{ color: "white" }}>
+							ID
+						</TableCell>
+						<TableCell align="center" sx={{ color: "white" }}>
+							Patient Name
+						</TableCell>
+						<TableCell align="center" sx={{ color: "white" }}>
+							Age
+						</TableCell>
+						<TableCell align="center" sx={{ color: "white" }}>
+							Gender
+						</TableCell>
+						<TableCell align="center" sx={{ color: "white" }}>
+							Profile
+						</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{users.map((row, index) => (
+						<TableRow
+							key={index}
+							sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+						>
+							<TableCell align="center" component="th" scope="row">
+								{row._id}
+							</TableCell>
+							<TableCell align="center">{row.firstName}</TableCell>
+							<TableCell align="center">{row.age}</TableCell>
+							<TableCell align="center">{row.gender}</TableCell>
+							<TableCell align="center">
+								<div
+									onClick={() => {
+										navigate("/admin/patients/profile", {
+											state: { patient: row._id },
+										})
+									}}
+								>
+									<SmallButton value="Profile" />
+								</div>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	)
+}
--- src/pages/adminPages/patients.js
+++ src/pages/adminPages/patients.js
@@ -1,12 +1,16 @@
-import { Container, Typography } from "@mui/material"
+import { Container, TextField, Typography } from "@mui/material"
 import axios from "../../axios"
 import React, { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import AdminLayout from "../../layouts/AdminLayout"
+import PatientTable from "../../components/adminModule/patientTable"
+import { Box } from "@mui/system"
 
 const Patients = () => {
 	const adminState = useSelector((storeState) => storeState.admin)
 	const [data, setData] = useState([])
+	const [result, setResult] = useState([])
+	const [searchValue, setSearchValue] = useState("")
 	useEffect(() => {
 		;(async function() {
 			try {
@@ -21,6 +25,22 @@ const Patients = () => {
 			}
 		})()
 	}, [])
+
+	const handleSearch = (event) => {
+		event.preventDefault()
+		const search = new FormData(event.currentTarget)
+		setSearchValue(search.get("search").toLowerCase())
+		const searchResult = data.filter((o) =>
+			Object.entries(o).some((entry) =>
+				String(entry[1])
+					.toLowerCase()
+					.includes(searchValue)
+			)
+		)
+		console.log(searchResult)
+		setResult(searchResult)
+	}
+
 	return (
 		<AdminLayout>
 			<Container>
@@ -37,6 +57,44 @@ const Patients = () => {
 				>
 					Patients
 				</Typography>
+				<Typography
+					sx={{
+						fontSize: {
+							xs: "1.2rem",
+							sm: "1.4rem",
+						},
+						fontFamily: "sans-serif",
+						color: "#595959",
+						textAlign: "center",
+						mb: 3,
+						mt: { xs: 1, sm: 3 },
+					}}
+					component="p"
+				>
+					Users list
+				</Typography>
+				<Box
+					display="flex"
+					justifyContent="center"
+					component="form"
+					autoComplete="off"
+					onChange={handleSearch}
+					onSubmit={handleSearch}
+					noValidate
+					sx={{ mt: 1, mb: 2 }}
+				>
+					<TextField
+						margin="normal"
+						fullWidth
+						id="search"
+						label="Search"
+						name="search"
+						autoFocus
+						variant="standard"
+						sx={{ width: "90%" }}
+					/>
+				</Box>
+				<PatientTable users={searchValue === "" ? data : result} />
 			</Container>
 		</AdminLayout>
 	)
