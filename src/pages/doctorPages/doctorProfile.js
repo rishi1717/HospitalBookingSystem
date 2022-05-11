0a10fe4b7bd55ed3f5a76367644194748139c861
rishi1717 <rshrrrss@gmail.com>
2022-05-11 10:53:36 +0530

    doctor password changed

--- src/pages/doctorPages/doctorProfile.js
+++ src/pages/doctorPages/doctorProfile.js
@@ -3,16 +3,39 @@ import Typography from "@mui/material/Typography"
 import Card from "@mui/material/Card"
 import CardContent from "@mui/material/CardContent"
 import CardMedia from "@mui/material/CardMedia"
-import { Box, Grid } from "@mui/material"
+import { Box, Grid, TextField, Modal, Container } from "@mui/material"
 import DoctorsLayout from "../../layouts/DoctorsLayout"
-import { MediumButton } from "../../components/Buttons"
+import { SmallButton } from "../../components/Buttons"
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import axios from "../../axios"
 import Unauthorized from "./unauthorized"
+import Swal from "sweetalert2"
+
+const Toast = Swal.mixin({
+	background: "#1E1E1E",
+	color: "white",
+	toast: true,
+	position: "top-end",
+	showConfirmButton: false,
+	timerProgressBar: true,
+})
+
+const style = {
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: "60vw",
+	bgcolor: "background.paper",
+	borderRadius: "4px",
+	boxShadow: 24,
+	p: 4,
+}
 
 function DoctorProfile() {
 	const docState = useSelector((storeState) => storeState.doctor)
+
 	if (docState.token) {
 		const [doctor, setDoctor] = useState({
 			image: "",
@@ -32,6 +55,56 @@ function DoctorProfile() {
 				if (doctorData.data.doctor) setDoctor(doctorData.data.doctor)
 			})()
 		}, [])
+
+		const [data, setData] = useState({
+			oldPassword: "",
+			newPassword: "",
+			cPassword: "",
+		})
+		const [error, setError] = useState("")
+		const [open, setOpen] = React.useState(false)
+		const handleOpen = () => {
+			setOpen(true)
+		}
+		const handleClose = () => {
+			setOpen(false)
+		}
+
+		const handleChange = (e) => {
+			setData({ ...data, [e.target.name]: e.target.value })
+		}
+
+		const handleSubmit = async (e) => {
+			e.preventDefault()
+			if (data.newPassword.length < 8) {
+				setError("Password need to be atleast 8 digits")
+				return
+			}
+			if (data.newPassword !== data.cPassword) {
+				setError("Passwords do not match")
+				return
+			}
+			try {
+				await axios.put(
+					`/doctor/changepassword/${docState.id}`,
+					data,
+					{
+						headers: { "auth-token": docState.token },
+					}
+				)
+				Toast.fire({
+					position: "bottom-right",
+					icon: "success",
+					title: "Password changed",
+					showConfirmButton: false,
+					timer: 3000,
+				})
+				handleClose()
+			} catch (err) {
+				setError(err.response.data.message)
+			}
+		}
+
 		return (
 			<DoctorsLayout>
 				<Typography
@@ -139,6 +212,21 @@ function DoctorProfile() {
 									}}
 									component="p"
 								>
+									<b>Email :</b> {doctor.email}
+								</Typography>
+								<Typography
+									sx={{
+										mt: 2,
+										fontSize: {
+											xs: "1rem",
+											sm: "1rem",
+											textAlign: "left",
+										},
+										fontFamily: "sans-serif",
+										color: "#595959",
+									}}
+									component="p"
+								>
 									<b>Qualification :</b> {doctor.qualification}
 								</Typography>
 								<Typography
@@ -204,17 +292,109 @@ function DoctorProfile() {
 								</Typography>
 							</CardContent>
 						</Grid>
-						<div style={{ marginLeft: "auto", marginTop: 2 }}>
+						<div style={{ display:'flex', marginLeft: "auto", marginTop: 2 }}>
+							<div
+								onClick={async () => {
+									handleOpen()
+								}}
+							>
+								<SmallButton value="Change Password" color="#595959" />
+							</div>
 							<Link
 								style={{ textDecoration: "none" }}
 								state={{ doctor: doctor }}
 								to="/doctor/Profile/edit"
 							>
-								<MediumButton value="Edit" color="#595959" />
+								<SmallButton value="Edit" color="#595959" />
 							</Link>
 						</div>
 					</Grid>
 				</Card>
+				<Modal
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="modal-modal-title"
+					aria-describedby="modal-modal-description"
+				>
+					<Box sx={style} component="form" onSubmit={handleSubmit}>
+						<Typography
+							sx={{
+								textAlign: "center",
+								fontSize: {
+									xs: "1rem",
+									sm: "1rem",
+								},
+								fontFamily: "sans-serif",
+								color: "#1976D2",
+							}}
+							component="p"
+						>
+							Change Password
+						</Typography>
+						<Container sx={{ p: { xs: 0, sm: 3 } }}>
+							<TextField
+								sx={{ mt: 1 }}
+								required
+								fullWidth
+								name="oldPassword"
+								label="Old Password"
+								type="password"
+								id="oldPassword"
+								onChange={handleChange}
+								value={data.oldPassword}
+							/>
+							<TextField
+								sx={{ mt: 1 }}
+								required
+								fullWidth
+								name="newPassword"
+								label="New Password"
+								type="password"
+								id="newPassword"
+								onChange={handleChange}
+								value={data.newPassword}
+							/>
+							<TextField
+								sx={{ mt: 1 }}
+								required
+								fullWidth
+								name="cPassword"
+								label="Confirm Password"
+								type="password"
+								id="cPassword"
+								onChange={handleChange}
+								value={data.cPassword}
+							/>
+							{error && (
+								<Typography
+									sx={{
+										mt: 1,
+										color: "red",
+										fontSize: "0.8rem",
+										textAlign: "center",
+									}}
+								>
+									{error}
+								</Typography>
+							)}
+							<Grid container sx={{ mt: 1 }}>
+								<div onClick={handleClose}>
+									<SmallButton
+										value="Cancel"
+										color="#eaeaea"
+										text="#CC3E34"
+									/>
+								</div>
+								<SmallButton
+									type="submit"
+									color="#eaeaea"
+									text="#609acf"
+									value="Change Password"
+								/>{" "}
+							</Grid>
+						</Container>
+					</Box>
+				</Modal>
 			</DoctorsLayout>
 		)
 	} else {
