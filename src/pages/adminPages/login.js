ceac93b94153e4799c0781fea06a00f02be13944
rishi1717 <rshrrrss@gmail.com>
2022-05-15 15:32:08 +0530

    login updated admin

--- src/pages/adminPages/login.js
+++ src/pages/adminPages/login.js
@@ -2,8 +2,6 @@ import * as React from "react"
 import Avatar from "@mui/material/Avatar"
 import Button from "@mui/material/Button"
 import TextField from "@mui/material/TextField"
-import FormControlLabel from "@mui/material/FormControlLabel"
-import Checkbox from "@mui/material/Checkbox"
 import Link from "@mui/material/Link"
 import Box from "@mui/material/Box"
 import Grid from "@mui/material/Grid"
@@ -165,12 +163,6 @@ export default function Login() {
 								<Typography sx={{ color: "red", m: 1 }}>
 									{error ? error : ""}
 								</Typography>
-								<FormControlLabel
-									control={
-										<Checkbox value="remember" color="primary" />
-									}
-									label="Remember me"
-								/>
 								<Button
 									type="submit"
 									fullWidth
